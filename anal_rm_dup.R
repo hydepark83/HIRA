4bcf145dd0ab3cd1896e65d91297dbdd8c9bcbd8
JC <hydepark83@gmail.com>
2014-01-01 14:29:10 +0000

    new updates

--- anal_rm_dup.R
+++ anal_rm_dup.R
@@ -1,6 +1,9 @@
 library(sm)
 library(data.table)
 
+YNO.data <- read.table("../data/ykiho.txt", sep=",", header=T)
+YNO.dt <- data.table(YNO.data)
+setkey(YNO.dt, YNO)
 ## Load data
 #r.data <- read.table("./data/proc_ED_nps_20", header=T,  sep=",") #2009.01.01 ~ 2009.12.31
 load("../data/proc_ED_nps_20.rda")
@@ -51,8 +54,89 @@ num.ed.patients.all <- length(FF.NO) + length(NO.FF.NO)
 # >num.ed.patients.FF/num.ed.patients.all
 # >0.0309447 <<<<< CH
 
-# data structure conversion for boost computational speed
+### Trend ED visits vs. Disease
+temp.DN <- list()
+for(edc in c(1:10)){
+	print(edc)
+	temp.NO <- names(table(ed.count.data$NO)[which(table(ed.count.data$NO)==edc)])
+	if(edc==10){
+		temp.NO <- names(table(ed.count.data$NO)[which(table(ed.count.data$NO)>=edc & table(ed.count.data$NO)<=26)])
+		}
+	flush <- apply(as.matrix(temp.NO), 1, temp <- function(nn){
+				temp <- as.matrix(ed.dt[J(as.numeric(nn))])
+				temp.main.sick <- unique(temp[,"MAIN_SICK"])
+				return(temp.main.sick)
+				})	
+	temp.DN[[edc]] <- unlist(flush)
+	}
 
+## frequency >1000
+DN.all <- names(table(r.data[,"MAIN_SICK"]))
+rs <- c()
+for(dn in DN.all){
+	temp.prop <- c()
+	for(edc in c(1:10)){
+		temp.prop <- c(temp.prop, length(which(temp.DN[[edc]]== dn))/length(temp.DN[[edc]]))
+		}
+	num.ed.visit <- c(1:10)
+	temp.rs <- c(dn, anova(lm(num.ed.visit~temp.prop))[[5]][1])
+	rs <- rbind(rs, temp.rs)
+	}
+rs.adjp <- p.adjust(as.numeric(rs[,2]))
+rs <- cbind(rs, rs.adjp)
+rs <- rs[sort(as.numeric(rs[,2]), decreasing=F, index.return=T)$ix,]
+
+## Disease significantly associated with ED visits
+sig.d <- rs[which(as.numeric(rs[,3])<0.05),1]
+for(dn in sig.d){
+	temp.prop <- c()
+        for(edc in c(1:10)){
+                temp.prop <- c(temp.prop, length(which(temp.DN[[edc]]== dn))/length(temp.DN[[edc]]))
+                }
+	num.ed.visit <- c(c(1:9), ">10")
+	print(cbind(num.ed.visit, temp.prop))
+	}
+length(DN.all)
+#8425
+#rs[which(as.numeric(rs[,3])<0.05),]
+#temp.rs "F102" "1.65509315088692e-07" "0.000871241034626875"
+#temp.rs "I219" "9.31761339711051e-06" "0.0490385993089926" 
+
+## F102
+## Chronic alcoholism
+## Alcohol addiction
+## Mental and behavioral disorders due to alcohol dependence syndrome
+
+# [1,] "1"          "0.00139041005445125"
+# [2,] "2"          "0.00343665322847135"
+# [3,] "3"          "0.0081573896353167" 
+# [4,] "4"          "0.0108108108108108" 
+# [5,] "5"          "0.0168654874537227" 
+# [6,] "6"          "0.0231124807395994" 
+# [7,] "7"          "0.0335365853658537" 
+# [8,] "8"          "0.0328849028400598" 
+# [9,] "9"          "0.0349344978165939" 
+#[10,] ">10"        "0.0441603718768158" 
+
+# I219 
+#Acute myocardial infarction, unspecified
+#Myocardial infarction(acute) NOS
+      
+#num.ed.visit temp.prop            
+# [1,] "1"          "0.00653259695974025"
+# [2,] "2"          "0.00650981428854669"
+# [3,] "3"          "0.00575815738963532"
+# [4,] "4"          "0.00627027027027027"
+# [5,] "5"          "0.00411353352529823"
+# [6,] "6"          "0.00385208012326656"
+# [7,] "7"          "0.00304878048780488"
+# [8,] "8"          "0.00149476831091181"
+# [9,] "9"          "0.00218340611353712"
+#[10,] ">10"        "0"  	
+
+
+
+# data structure conversion for boost computational speed
 print(Sys.time())
 FF.info <- apply(as.matrix(FF.NO), 1, temp <- function(nn){
 		temp <- as.matrix(ed.dt[J(as.numeric(nn))])
@@ -64,6 +148,7 @@ FF.info <- apply(as.matrix(FF.NO), 1, temp <- function(nn){
                 temp.self.pay <- temp.total.pay - temp.insurance.pay
 		# there is an issue in DMD_SBRDN_AMT columns (all zero in some cases)
 		temp.dur <- sum(as.numeric(temp[,"RECN"]))/nrow(temp)
+		temp.vscn <- sum(as.numeric(temp[,"VSCN"]))/nrow(temp)
 		temp.se.class <- temp[,"MACL_CD"][1]
 		temp.main.sick <- paste(unique(temp[,"MAIN_SICK"]), collapse="|")
 		tt <- temp[,"DIAG_RSLT_TYPE"]
@@ -80,8 +165,19 @@ FF.info <- apply(as.matrix(FF.NO), 1, temp <- function(nn){
                         temp.comorbidity.idx=0
 			temp.comorbidity=NA
                         }
+		if(length(which(temp[,"OPR_YN"]==9))>0){
+			temp.opr <- 1
+			}else{
+			temp.opr <- 0
+			}
+		kk <- temp[,"YNO"]
+		temp.YNO <- apply(as.matrix(kk), 1, temp <- function(yyn){
+					temp.yno <- YNO.dt[J(as.numeric(yyn))]
+					return(temp.yno[,bed_grade])}
+					)
+		temp.YNO <- mean(temp.YNO)
 		temp.outPD.visit <- nrow(r.dt[J(as.numeric(nn),0),nomatch=0])
-		return(c(nn, temp.age, temp.gen, temp.insurance.pay, temp.self.pay, temp.dur, temp.se.class, temp.main.sick, temp.death, temp.comorbidity.idx, temp.comorbidity, temp.outPD.visit))
+		return(c(nn, temp.age, temp.gen, temp.insurance.pay, temp.self.pay, temp.dur, temp.se.class, temp.main.sick, temp.death, temp.comorbidity.idx, temp.comorbidity, temp.outPD.visit, temp.vscn, temp.opr, temp.YNO))
 		})
 print(Sys.time())
 
@@ -95,6 +191,7 @@ NO.FF.info <- apply(as.matrix(NO.FF.NO), 1, temp <- function(nn){
 	                temp.total.pay <- sum(as.numeric(temp[,"DMD_TRAMT"]))/nrow(temp)
 	                temp.self.pay <- temp.total.pay - temp.insurance.pay
 			temp.dur <- sum(as.numeric(temp[,"RECN"]))/nrow(temp)
+			temp.vscn <- sum(as.numeric(temp[,"VSCN"]))/nrow(temp)
 			temp.se.class <- temp[,"MACL_CD"][1]
 			temp.main.sick <- paste(unique(temp[,"MAIN_SICK"]), collapse="|")
 	                tt <- temp[,"DIAG_RSLT_TYPE"]
@@ -111,6 +208,17 @@ NO.FF.info <- apply(as.matrix(NO.FF.NO), 1, temp <- function(nn){
                         	temp.comorbidity.idx=0
 				temp.comorbidity=NA
                         	}
+			if(length(which(temp[,"OPR_YN"]==9))>0){
+                        temp.opr <- 1
+                        }else{
+                        temp.opr <- 0
+                        }
+			kk <- temp[,"YNO"]
+	                temp.YNO <- apply(as.matrix(kk), 1, temp <- function(yyn){
+                                        temp.yno <- YNO.dt[J(as.numeric(yyn))]
+                                        return(temp.yno[,bed_grade])}
+                                        )
+        	        temp.YNO <- mean(temp.YNO)
 			}else{
 			names(temp) <- colnames(ed.data)
 			temp.age <- as.numeric(temp["age"])
@@ -119,6 +227,7 @@ NO.FF.info <- apply(as.matrix(NO.FF.NO), 1, temp <- function(nn){
 			temp.total.pay <-as.numeric(temp["DMD_TRAMT"])
 			temp.self.pay <- temp.total.pay - temp.insurance.pay
 			temp.dur <- as.numeric(temp["RECN"])
+			temp.vscn <- as.numeric(temp["VSCN"])
 			temp.se.class <- temp["MACL_CD"]
 			temp.main.sick <- temp["MAIN_SICK"]
 				
@@ -136,15 +245,23 @@ NO.FF.info <- apply(as.matrix(NO.FF.NO), 1, temp <- function(nn){
                                 temp.comorbidity.idx=0
 				temp.comorbidity=NA
                                 }
+			if(temp["OPR_YN"]==9){
+                        temp.opr <- 1
+                        }else{
+                        temp.opr <- 0
+                        }
+                	yyn <-temp["YNO"]
+                        temp.yno <- YNO.dt[J(as.numeric(yyn))]
+                        temp.YNO <- temp.yno[,bed_grade]
 			}
 		temp.outPD.visit <- nrow(r.dt[J(as.numeric(nn),0),nomatch=0])
-                return(c(nn, temp.age, temp.gen, temp.insurance.pay, temp.self.pay, temp.dur, temp.se.class, temp.main.sick, temp.death, temp.comorbidity.idx, temp.comorbidity, temp.outPD.visit))
+		return(c(nn, temp.age, temp.gen, temp.insurance.pay, temp.self.pay, temp.dur, temp.se.class, temp.main.sick, temp.death, temp.comorbidity.idx, temp.comorbidity, temp.outPD.visit, temp.vscn, temp.opr, temp.YNO))
                 })
 print(Sys.time())
 
 
 FF.info <- t(FF.info); FF.info <- as.data.frame(FF.info)
-colnames(FF.info) <- c("NO", "age", "gen", "insurance.pay", "patient.pay", "days.in.hospitals", "class", "main.sick", "death", "comorbidity.idx", "comorbidity", "out.patient.department.visits")
+colnames(FF.info) <- c("NO", "age", "gen", "insurance.pay", "patient.pay", "days.in.hospitals", "class", "main.sick", "death", "comorbidity.idx", "comorbidity", "out.patient.department.visits", "treat.days", "OPRYN", "YNO")
 FF.info$age <- as.numeric(as.matrix(FF.info$age))
 FF.info$insurance.pay <- as.numeric(as.matrix(FF.info$insurance.pay))
 FF.info$patient.pay <- as.numeric(as.matrix(FF.info$patient.pay))
@@ -152,9 +269,15 @@ FF.info$days.in.hospitals <- as.numeric(as.matrix(FF.info$days.in.hospitals))
 FF.info$main.sick <- as.character(as.matrix(FF.info$main.sick))
 FF.info$comorbidity <- as.character(as.matrix(FF.info$comorbidity))
 FF.info$out.patient.department.visits <- as.numeric(as.matrix(FF.info$out.patient.department.visits))
+FF.info$treat.days <- as.numeric(as.matrix(FF.info$treat.days))
+FF.info$YNO <- as.numeric(as.matrix(FF.info$YNO))
+FF.info$gen <- as.factor(as.matrix(FF.info$gen))
+FF.info$class <- as.factor(as.matrix(FF.info$class))
+FF.info$death <- as.factor(as.matrix(FF.info$death))
+FF.info$OPRYN <- as.factor(as.matrix(FF.info$OPRYN))
 
 NO.FF.info <- t(NO.FF.info); NO.FF.info <- data.frame(NO.FF.info)
-colnames(NO.FF.info) <- c("NO", "age", "gen", "insurance.pay", "patient.pay", "days.in.hospitals", "class", "main.sick", "death", "comorbidity.idx", "comorbidity", "out.patient.department.visits")
+colnames(NO.FF.info) <- c("NO", "age", "gen", "insurance.pay", "patient.pay", "days.in.hospitals", "class", "main.sick", "death", "comorbidity.idx", "comorbidity", "out.patient.department.visits", "treat.days", "OPRYN", "YNO")
 NO.FF.info$age <- as.numeric(as.matrix(NO.FF.info$age))
 NO.FF.info$insurance.pay <- as.numeric(as.matrix(NO.FF.info$insurance.pay))
 NO.FF.info$patient.pay <- as.numeric(as.matrix(NO.FF.info$patient.pay))
@@ -162,6 +285,12 @@ NO.FF.info$days.in.hospitals <- as.numeric(as.matrix(NO.FF.info$days.in.hospital
 NO.FF.info$main.sick <- as.character(as.matrix(NO.FF.info$main.sick))
 NO.FF.info$comorbidity <- as.character(as.matrix(NO.FF.info$comorbidity))
 NO.FF.info$out.patient.department.visits <- as.numeric(as.matrix(NO.FF.info$out.patient.department.visits))
+NO.FF.info$treat.days <- as.numeric(as.matrix(NO.FF.info$treat.days))
+NO.FF.info$YNO <- as.numeric(as.matrix(NO.FF.info$YNO))
+NO.FF.info$gen <- as.factor(as.matrix(NO.FF.info$gen))
+NO.FF.info$class <- as.factor(as.matrix(NO.FF.info$class))
+NO.FF.info$death <- as.factor(as.matrix(NO.FF.info$death))
+NO.FF.info$OPRYN <- as.factor(as.matrix(NO.FF.info$OPRYN))
 
 t.test(FF.info[,"out.patient.department.visits"]);t.test(NO.FF.info[,"out.patient.department.visits"])
 t.test(FF.info[,"out.patient.department.visits"], NO.FF.info[,"out.patient.department.visits"])
@@ -169,6 +298,52 @@ t.test(FF.info[,"age"]);t.test(NO.FF.info[,"age"])
 t.test(FF.info[,"age"], NO.FF.info[,"age"])
 gender.table <- rbind(table(FF.info[,"gen"]), table(NO.FF.info[,"gen"])); gender.table
 chisq.test(gender.table)
+
+##3 NEW!!!
+t.test(FF.info[,"treat.days"], NO.FF.info[,"treat.days"])
+t.test(FF.info[,"YNO"], NO.FF.info[,"YNO"])
+opryn.table <- rbind(table(FF.info[,"OPRYN"]), table(NO.FF.info[,"OPRYN"]))
+chisq.test(opryn.table)
+
+### Multiple logistic regression
+mr.data <- cbind(rbind(FF.info, NO.FF.info), FREQ.EDusers=c(rep(1, length(FF.info)), rep(0, length(NO.FF.info))))
+mr.data <- mr.data[,c("FREQ.EDusers", "age", "gen", "days.in.hospitals", "class", "death", "out.patient.department.visits", "treat.days", "OPRYN", "YNO", "YNO")]
+mylogit <- glm(FREQ.EDusers~., data=mr.data, family = "binomial")
+summary(mylogit)
+confint.default(mylogit)
+exp(coef(mylogit)) #OR
+exp(cbind(OR = coef(mylogit), confint(mylogit)))
+
+### Prediction
+library(ROCR)
+# 10 permutation
+bitmap("ROCcurves.png", res=300)
+p.auc <- c()
+for(i in c(1:10)){
+	p.idx <- sample(c(1:nrow(mr.data)), nrow(mr.data), replace=T)
+	p.mr.data<- mr.data[p.idx,]
+	p.pred <- predict(mylogit, p.mr.data, type="response")
+	preds<- prediction(as.numeric(p.pred), as.numeric(p.mr.data[,"FREQ.EDusers"]))
+	perf <- performance(preds, "tpr", "fpr") 
+	perf2 <- performance(preds, "auc") 
+	p.auc <- c(p.auc, perf2)
+	if(i==1){
+		plot(perf, main="ROC Curves from ten permutations",  lwd=2, col="darkgrey")
+		}else{
+		lines(perf, lwd=2, col="darkgrey")
+		}
+	}
+legend("bottomright", paste("Average AUC values: ", round(mean(p.auc), 1), sep=""), col="red", cex=1)
+graphics.off()
+
+	
+
+
+
+
+
+
+
 #medical protection proportion
 length(which(is.na(FF.info$class)==F))/nrow(FF.info)
 length(which(is.na(NO.FF.info$class)==F))/nrow(NO.FF.info)
